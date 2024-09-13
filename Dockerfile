3259939b680549d82cccb6011fd3ed57058974b7
celestix <i@anodite.me>
2024-09-14 00:56:35 +0530

    create a form for qna

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ USER root
 COPY req.txt /root/req.txt
 COPY gen_flag /root/gen_flag
 COPY main.py /root/main.py
+COPY templates /root/templates
 RUN chmod +x /root/gen_flag
 # RUN /bin/bash /root/gen_flag c0f{something_somethin}
 WORKDIR /root
--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-import os
-from flask import Flask
+from flask import Flask, request, render_template
 
 FLAG = "c0d{fake_flag}"
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return FLAG
+qna = {
+    "dob": "May 26, 1974",
+    "org": "Satara, Maharashtra, India",
+    "mcycle": "Royal Enfield Classic 500",
+    "cuisine": "Italian cuisine",
+    "curr": "Stockholm, Sweden",
+    "track": "Tarmac",
+    "degree": "Mechanical Engineering",
+    "rally": "WRC EKO Acropolis Rally Greece",
+    "next": "WRC Rally Chile Bio Bio",
+    "team": "Subaru World Rally Team (SWRT)",
+}
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        for key, value in qna.items():
+            if request.form.get(key) != value:
+                return "Wrong answer! Try again."
+        return FLAG
+
+    return render_template("ques.html")
 
 app.run('0.0.0.0', 8080)
